622d17c9c4cc59cb1995b9daa1090454be85bb9c
Edhic1 <g130352027@taalim.ma>
2023-04-22 17:01:56 +0000

    modified:   gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,6 +2,9 @@
 WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
 
 i use gunicorn for production deployment WSGI server
+
+for run it do : gunicorn app:app -c gunicorn_config.py
+
 """
 
 bind = '127.0.0.1:8000'  # Replace with the IP address and port number you want to use
