a4b9e2c2a5415fbb07318698d5435a2b1e14d1f7
Edhic1 <g130352027@taalim.ma>
2023-06-05 14:23:45 +0100

    make job for date

--- procedure.sql
+++ procedure.sql
@@ -8,3 +8,44 @@ BEGIN
     WHERE IDTACHE = IDTACHE_IN;
 END;
 /
+
+
+
+-- make execute every DAY
+
+BEGIN
+  DBMS_SCHEDULER.CREATE_JOB (
+    job_name        => 'update_etatproj_job',
+    job_type        => 'EXECUTABLE',
+    job_action      => '/opt/oracle/instantclient_21_4/sqlplus /nolog @/home/hicham/update_etatproj.sql',
+    start_date      => SYSTIMESTAMP,
+    repeat_interval => 'FREQ=DAILY;BYHOUR=0;BYMINUTE=0;BYSECOND=0',
+    enabled         => TRUE
+  );
+END;
+/
+
+-- make it execute every minute
+
+BEGIN
+  DBMS_SCHEDULER.CREATE_JOB (
+    job_name        => 'update_etatproj_job',
+    job_type        => 'EXECUTABLE',
+    job_action      => '/opt/oracle/instantclient_21_4/sqlplus /nolog @/home/hicham/update_etatproj.sql',
+    start_date      => SYSTIMESTAMP,
+    repeat_interval => 'FREQ=MINUTELY;INTERVAL=1',
+    enabled         => TRUE
+  );
+END;
+/
+
+
+--- to drop it 
+
+BEGIN
+  DBMS_SCHEDULER.DROP_JOB (
+    job_name        => 'update_etatproj_job',
+    force           => FALSE
+  );
+END;
+/
